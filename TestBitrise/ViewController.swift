d49f7f19cf1f86c57b667b312cf4d2034f84a635
BENJ.HEDI <hbenjahouach.webnet@tf1.fr>
2018-07-18 14:28:19 +0200

    create project

---
+++ TestBitrise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestBitrise
+//
+//  Created by BENJ.HEDI on 18/07/2018.
+//  Copyright © 2018 ETF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
